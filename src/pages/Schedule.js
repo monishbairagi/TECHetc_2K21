3236f350647f5c3e1747938d6823758309147074
monishbairagi <monishbairagi@gmail.com>
2021-08-29 21:41:26 +0100

    schedule updated

--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -11,87 +11,87 @@ export const Schedule = () => {
         },
         {
             id: "1",
-            event: "QUIZZARD",
-            day1: "10:30 AM - 5:00 PM",
-            day2: "10:30 AM - 5:00 PM"
+            event: "MATHMANIA",
+            day1: "10:00 AM",
+            day2: "X"
         },
         {
             id: "2",
-            event: "HUNT FOR FUN",
-            day1: "X",
-            day2: "10:30 AM - 5:00 PM1"
+            event: "CHESS (SCHOOL & COLLEGE)",
+            day1: "11:00 AM",
+            day2: "X"
         },
         {
             id: "3",
-            event: "SUBWAY SURFERS",
-            day1: "X",
-            day2: "10:30 AM - 5:00 PM"
+            event: "CODE RESTYLE",
+            day1: "12:00 PM",
+            day2: "X"
         },
         {
             id: "4",
-            event: "CODE RESTYLE",
-            day1: "10:30 AM - 5:00 PM",
+            event: "LOGI-CAD",
+            day1: "12:00 PM",
             day2: "X"
         },
         {
             id: "5",
-            event: "MATHMANIA",
-            day1: "10:30 AM - 5:00 PM",
-            day2: "2:30 PM - 5:00 PM"
+            event: "CIVIL-O-PEDIA",
+            day1: "12:00 PM",
+            day2: "X"
         },
         {
-            id: "5",
-            event: "MATHMANIA",
-            day1: "10:30 AM - 5:00 PM",
-            day2: "2:30 PM - 5:00 PM"
+            id: "6",
+            event: "μ-MAT",
+            day1: "1:30 PM",
+            day2: "X"
         },
         {
-            id: "5",
-            event: "MATHMANIA",
-            day1: "10:30 AM - 5:00 PM",
-            day2: "2:30 PM - 5:00 PM"
+            id: "7",
+            event: "CIRCUI-T-RICS",
+            day1: "3:00 PM",
+            day2: "X"
         },
         {
-            id: "5",
-            event: "MATHMANIA",
-            day1: "10:30 AM - 5:00 PM",
-            day2: "2:30 PM - 5:00 PM"
+            id: "8",
+            event: "MATHMANIA (SCHOOL)",
+            day1: "X",
+            day2: "11:00 AM"
         },
         {
-            id: "5",
-            event: "MATHMANIA",
-            day1: "10:30 AM - 5:00 PM",
-            day2: "2:30 PM - 5:00 PM"
+            id: "9",
+            event: "CIRCUITECH",
+            day1: "X",
+            day2: "10:00 AM"
         },
         {
-            id: "5",
-            event: "MATHMANIA",
-            day1: "10:30 AM - 5:00 PM",
-            day2: "2:30 PM - 5:00 PM"
+            id: "10",
+            event: "INSTA-PLAN",
+            day1: "X",
+            day2: "11:00 AM"
         },
         {
-            id: "5",
-            event: "MATHMANIA",
-            day1: "10:30 AM - 5:00 PM",
-            day2: "2:30 PM - 5:00 PM"
+            id: "11",
+            event: "8 BALL POOL",
+            day1: "X",
+            day2: "12:00 PM"
         },
         {
-            id: "5",
-            event: "MATHMANIA",
-            day1: "10:30 AM - 5:00 PM",
-            day2: "2:30 PM - 5:00 PM"
+            id: "12",
+            event: "CODE-2-DUO",
+            day1: "X",
+            day2: "1:00 PM"
         },
         {
-            id: "5",
-            event: "MATHMANIA",
-            day1: "10:30 AM - 5:00 PM",
-            day2: "2:30 PM - 5:00 PM"
+            id: "13",
+            event: "HUNT FOR FUN",
+            day1: "X",
+            day2: "2:00 PM"
         },
         {
-            id: "5",
-            event: "Last Game",
-            day1: "10:30 AM - 5:00 PM",
-            day2: "2:30 PM - 5:00 PM"
+            id: "14",
+            event: "CROSSWORD",
+            day1: "X",
+            day2: "4:00 PM"
         }
     ]
     return (
